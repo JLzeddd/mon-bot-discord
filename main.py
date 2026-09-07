45b745d5744220a89b59367838efe36c72564bff
JLzeddd <louis.sebti2110@gmail.com>
2026-09-07 19:03:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import discord
 from discord.ext import commands
 from gtts import gTTS
 import os
+from dotenv import load_dotenv
 import asyncio
 import random
 import datetime
 import shutil
 import subprocess
+
+load_dotenv()
 
 # Installe FFmpeg au démarrage
 async def setup_ffmpeg():
